a593544ce072d566494495a995bddc1b43f213aa
Jitulteron7 <jitulteron9@gmail.com>
2022-12-26 00:37:45 +0530

    marketing production webpack config done nad marketing

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -2,7 +2,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$|jsx/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
